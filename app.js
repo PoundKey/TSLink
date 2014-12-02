588c44fd1aee34a999d0e2f61d529bc5df4daed8
C.T Xue <xuecnt@hotmail.com>
2014-12-02 00:08:45 -0800

    underscore.js dependency added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var api = require('./routes/api');
+var _ = require('underscore');
 
 var app = express();
 
