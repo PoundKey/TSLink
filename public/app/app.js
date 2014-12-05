1fefd02d4d417c535155a5c4763baf61ad786118
C.T Xue <xuecnt@hotmail.com>
2014-12-05 03:43:37 -0800

    socket.io setup with angularjs factory updated

--- public/app/app.js
+++ public/app/app.js
@@ -6,47 +6,14 @@ var myApp = angular.module('myApp', [
   'ngRoute',
   'ngSanitize',
   'myApp.controllers',
-  "firebase"
+  "firebase",
+  'btford.socket-io'
 
 ]);
 
-myApp.factory('socket.io', function ($rootScope) {
-  var socket = io();
-  return {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          callback.apply(socket, args);
-        });
-      });
-    },
-    emit: function (eventName, data, callback) {
-      socket.emit(eventName, data, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          if (callback) {
-            callback.apply(socket, args);
-          }
-        });
-      })
-    }
-  };
+myApp.factory('socket.io', function (socketFactory) {
+  return socketFactory();
 });
 
-/**
-
-myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-	// if(window.history && window.history.pushState){
-	// 	$locationProvider.html5Mode(true);
-	// }
-  $routeProvider.when('/', {templateUrl: 'partials/overview.html', controller: 'AppCtrl'});
-  $routeProvider.when('/res/:id', {templateUrl: 'partials/data-template.html', controller: 'DataCtrl'});
-  $routeProvider.when('/profile', {templateUrl: 'partials/profile.html', controller: 'ProfileCtrl'});
-  $routeProvider.when('/chat', {templateUrl: 'partials/chat.html', controller: 'ChatCtrl'});
-  $routeProvider.otherwise({redirectTo: '/'});
-}]);
-
-*/
 
 
