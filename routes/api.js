082df066fb790b9515a6feafe1ced76a35b6d2f0
C.T Xue <xuecnt@hotmail.com>
2014-12-15 00:06:23 -0800

    tidy up template view codes

--- routes/api.js
+++ routes/api.js
@@ -10,33 +10,4 @@ router.get('/', function (req, res) {
     res.send('respond with a resource');
 });
 
-
-router.post('/handleBusStop', function (req, res) {
-	var apiGet = req.body.data;
-	request({
-		url: apiGet,
-		method: "GET",
-		timeout: 3000,
-    headers: {
-        Accept:'application/JSON'
-    }
-	}, function(error, response, body) {
-			if (error) {
-				console.log(error);
-				res.json({msg: 'Failed to fetch info for the given stop... /handleBusStop', err: error});
-			}
-			//console.log('Type: ' + typeof(infoArray));
-			//console.log(JSON.stringify(infoArray));
-			var infoArray = JSON.parse(body);
-			if (infoArray){
-				res.json({info:infoArray});
-			}else {
-				res.json({info:[]});
-			}
-
-	});
-});
-
-
-
 module.exports = router;
