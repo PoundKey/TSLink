22d171fd35af2b0d81c21838ced6dae0d25861b2
C.T Xue <xuecnt@hotmail.com>
2014-12-19 21:52:01 -0800

    readjusted the location view radius

--- routes/translink.js
+++ routes/translink.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-var apiKey = 'yDC04D3XtydprTHAeB0Z', count = 4, tf = 60, radius = 520;
+var apiKey = 'yDC04D3XtydprTHAeB0Z', count = 4, tf = 60, radius = 400;
 
 function Translink () {
 	//private instance variable goes here
